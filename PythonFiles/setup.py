c2eff268b67bb4212fbfb07d85339076284c7606
gabemast3r <113204875+gabemast3r@users.noreply.github.com>
2022-09-09 21:20:09 +0200

    Delete setup.py

--- PythonFiles/setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("slither.py")]
-
-cx_Freeze.setup(
-    name="Slither",
-    options={"build_exe":{"packages":["pygame"],"include_files":["apple.png","snakehead.png"]}},
-    description = "Slither",
-    executables = executables)
